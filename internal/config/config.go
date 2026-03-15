e0e9a32544846f5425a7b0eebb98154ea9434705
Sashko Guz <sashko.guz@gmail.com>
2026-03-15 21:47:11 +0200

    Add immutable to default Cache-Control header

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,21 +7,21 @@ import (
 )
 
 type Config struct {
-	StorageConfigPath   string
-	SignatureSecret     string
-	SignatureAlgorithm  string
-	SignatureStart      int
-	SignatureLength     int
-	Port                string
-	ReadTimeout         time.Duration
-	ReadHeaderTimeout   time.Duration
-	WriteTimeout        time.Duration
-	IdleTimeout         time.Duration
-	MaxHeaderBytes      int
-	MaxInputImageSize   int
-	MaxResizeWidth      int
-	MaxResizeHeight     int
-	MaxResizeResolution int
+	StorageConfigPath          string
+	SignatureSecret            string
+	SignatureAlgorithm         string
+	SignatureStart             int
+	SignatureLength            int
+	Port                       string
+	ReadTimeout                time.Duration
+	ReadHeaderTimeout          time.Duration
+	WriteTimeout               time.Duration
+	IdleTimeout                time.Duration
+	MaxHeaderBytes             int
+	MaxInputImageSize          int
+	MaxResizeWidth             int
+	MaxResizeHeight            int
+	MaxResizeResolution        int
 	CacheControlResponseHeader string
 }
 
@@ -31,22 +31,22 @@ func Load() *Config {
 	maxResizeResolution := getEnvInt("MAX_RESIZE_RESOLUTION", maxResizeWidth*maxResizeHeight)
 
 	return &Config{
-		StorageConfigPath:   getEnv("STORAGE_CONFIG_PATH", "./storage.json"),
-		SignatureSecret:     getEnv("SIGNATURE_SECRET", ""),
-		SignatureAlgorithm:  getEnv("SIGNATURE_ALGO", "sha256"),
-		SignatureStart:      getEnvIntMin("SIGNATURE_EXTRACT_START", 0, 0),
-		SignatureLength:     getEnvIntMin("SIGNATURE_LENGTH", 16, 1),
-		Port:                getEnv("PORT", "8080"),
-		ReadTimeout:         getEnvDurationSeconds("HTTP_READ_TIMEOUT_SECONDS", 5),
-		ReadHeaderTimeout:   getEnvDurationSeconds("HTTP_READ_HEADER_TIMEOUT_SECONDS", 2),
-		WriteTimeout:        getEnvDurationSeconds("HTTP_WRITE_TIMEOUT_SECONDS", 30),
-		IdleTimeout:         getEnvDurationSeconds("HTTP_IDLE_TIMEOUT_SECONDS", 120),
-		MaxHeaderBytes:      getEnvInt("HTTP_MAX_HEADER_BYTES", 1<<20),
-		MaxInputImageSize:   getEnvInt("MAX_INPUT_IMAGE_SIZE_MB", 64) * 1024 * 1024,
-		MaxResizeWidth:      maxResizeWidth,
-		MaxResizeHeight:     maxResizeHeight,
-		MaxResizeResolution: maxResizeResolution,
-		CacheControlResponseHeader: getEnv("CACHE_CONTROL_RESPONSE_HEADER", "public, max-age=31536000"),
+		StorageConfigPath:          getEnv("STORAGE_CONFIG_PATH", "./storage.json"),
+		SignatureSecret:            getEnv("SIGNATURE_SECRET", ""),
+		SignatureAlgorithm:         getEnv("SIGNATURE_ALGO", "sha256"),
+		SignatureStart:             getEnvIntMin("SIGNATURE_EXTRACT_START", 0, 0),
+		SignatureLength:            getEnvIntMin("SIGNATURE_LENGTH", 16, 1),
+		Port:                       getEnv("PORT", "8080"),
+		ReadTimeout:                getEnvDurationSeconds("HTTP_READ_TIMEOUT_SECONDS", 5),
+		ReadHeaderTimeout:          getEnvDurationSeconds("HTTP_READ_HEADER_TIMEOUT_SECONDS", 2),
+		WriteTimeout:               getEnvDurationSeconds("HTTP_WRITE_TIMEOUT_SECONDS", 30),
+		IdleTimeout:                getEnvDurationSeconds("HTTP_IDLE_TIMEOUT_SECONDS", 120),
+		MaxHeaderBytes:             getEnvInt("HTTP_MAX_HEADER_BYTES", 1<<20),
+		MaxInputImageSize:          getEnvInt("MAX_INPUT_IMAGE_SIZE_MB", 64) * 1024 * 1024,
+		MaxResizeWidth:             maxResizeWidth,
+		MaxResizeHeight:            maxResizeHeight,
+		MaxResizeResolution:        maxResizeResolution,
+		CacheControlResponseHeader: getEnv("CACHE_CONTROL_RESPONSE_HEADER", "public, max-age=31536000, immutable"),
 	}
 }
 
